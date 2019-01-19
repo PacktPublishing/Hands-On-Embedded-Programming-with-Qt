4b29f8f8772c6f06396011a305669d59e6d6c022
John Werner <jwernerny@gmail.com>
2019-01-19 00:02:37 -0500

    Code clean-up.

--- ch03/ch03-HelloFromQt_Final/mainwindow.cpp
+++ ch03/ch03-HelloFromQt_Final/mainwindow.cpp
@@ -18,7 +18,4 @@ void MainWindow::on_pushButton_clicked()
 {
     QString name = ui->lineEdit->text();
     ui->label->setText(QString("Nice to meet you %1!").arg(name));
-
-    static int ssCount = 0;
-    QTimer::singleShot(200, [this]() {this->grab().save(QString("screenshot_%1.png").arg(ssCount));});
 }
